eaed4cb5959bd69e2c6dbf4e8143ce8683d4c74b
${SilverfxA} <${silverfx@163.com}>
2021-10-10 10:07:14 +0800

    项目自身配置

--- Demo-model1/src/MainTest.java
+++ Demo-model1/src/MainTest.java
@@ -2,6 +2,6 @@ public class MainTest {
     public static void main(String[] args) {
         System.out.println("Hello Git");
         System.out.println("one");
-        System.out.println("change history");
+        System.out.println("change history ");
     }
 }
